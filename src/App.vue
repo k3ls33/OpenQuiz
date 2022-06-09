607372595191483c36c1add192de3fe29fbecf79
k3ls33 <kelseyllohr1@gmail.com>
2022-06-09 12:44:13 -0400

    Icons + license

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,24 @@
+<!--
+    OpenQuiz
+    Copyright (C) 2022 Kelsey Lohr
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+    ***
+
+    Icons by Icons8. See links in footer.
+-->
 <script>
 var url = 'https://opentdb.com/api.php?amount=10';
 import QuizQs from "./components/QuizQs.vue";
@@ -54,14 +75,35 @@ export default {
 
 <template id="app">
   <div v-if="!start">
-        <h1>OpenQuiz</h1>
-        <p>Fun and engaging classroom activities, open to all educators.</p>
-        <p>Try some of our current options below.
-            10 randomly selected questions each.
-        </p>
-        <button @click="category='science'"><img src="./assets/flask.png"/> Science</button>
-        <button @click="category='geography'"><img src="./assets/globe.png"/> Geography</button>
-        <button @click="category='history'"><img src="./assets/parchment.png"/> History</button>
+    <div>
+      <h1>OpenQuiz</h1>
+      <p>Fun and engaging classroom activities, open to all educators.</p>
+      <p>Try some of our current options below.
+        10 randomly selected questions each.
+      </p>
+    </div>
+
+    <div>
+      <button @click="category='science'">
+        <img src="./assets/test-tube.png" />Science
+      </button>
+
+      <button @click="category='geography'">
+        <img src="./assets/earth-planet.png" />Geography
+      </button>
+
+      <button @click="category='history'">
+        <img src="./assets/chichen-itza.png" />History
+      </button>
+    </div>
+
+    <div class="footer">
+    <a target="_blank" href="https://icons8.com/icon/CIP68rnmhjLL/test-tube">Test Tube</a>,
+    <a target="_blank" href="https://icons8.com/icon/v65sQ1D5r9Q2/earth-planet">Earth Planet</a>,
+     and
+    <a target="_blank" href="https://icons8.com/icon/h51PZi23TuuW/chichen-itza">Chichen Itza</a>
+     icons by <a target="_blank" href="https://icons8.com">Icons8</a>
+    </div>
   </div>
 
   <QuizQs v-if="start" :ok="start" :quizData="questions" :i="index"></QuizQs>
@@ -79,6 +121,8 @@ body {
 #app {
   text-align: center;
   margin-top: 30px;
+  width: 100%;
+  font-size: 18px;
 }
 
 .hideIntro {
@@ -90,11 +134,13 @@ button {
   border-radius: 15px;
   color: #ffffff;
   margin: 10px;
-  padding: 10px 15px;
+  padding-left: 15px;
+  padding-right: 15px;
   background-color: transparent;
   font-size: 20px;
   text-decoration: none;
   transition-duration: 0.3s;
+  height: 50px;
 }
 
 button:hover {
@@ -103,8 +149,23 @@ button:hover {
 }
 
 button img {
-  width: 20px;
-  display: inline;
+  height: 30px;
+  width: 30px;
   padding-right: 10px;
+  vertical-align: middle;
+}
+
+.footer {
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  padding-bottom: 20px;
+  font-size: 15px;
+}
+.footer a {
+  text-decoration: none;
+  font-weight:bold;
+  color: #72b9ff;
 }
 </style>
--- src/components/QuizQs.vue
+++ src/components/QuizQs.vue
@@ -1,3 +1,20 @@
+<!--
+    OpenQuiz
+    Copyright (C) 2022 Kelsey Lohr
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+-->
 <script>
 export default {
   name: "QuizQs",
@@ -42,8 +59,8 @@ export default {
       this.choices.sort(() => Math.random() - 0.5);
     },
     goNext() {
-      this.index++;
       this.next();
+      this.index++;
     }
   }
 }
@@ -52,7 +69,7 @@ export default {
 <template>
   <div id="quiz">
     <div v-if="ques.length">
-      <div id="question"><h1> {{ ques }} </h1></div>
+      <div id="question"><h2> {{ ques }} </h2></div>
 
       <div id="answerContainer">
         <ul>
@@ -69,7 +86,7 @@ export default {
     </div>
     
     <div v-else>
-      Ready?
+      <h2>Ready?</h2>
     </div>
     
     <button @click="goNext()">next</button>
