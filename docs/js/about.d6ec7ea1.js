7c90e52177a5d00a5fcc4a4a60d8e71de7705811
Kelsey L <kelseyllohr1@gmail.com>
2022-07-31 06:02:57 -0400

    build

--- docs/js/about.d6ec7ea1.js
+++ docs/js/about.d6ec7ea1.js
@@ -1,2 +1,2 @@
 "use strict";(self["webpackChunktrivia"]=self["webpackChunktrivia"]||[]).push([[443],{7381:function(n,t,a){a.r(t),a.d(t,{default:function(){return l}});var r=a(3396);const u={class:"about"},i=(0,r._)("h1",null,"This is an about page",-1),e=[i];function s(n,t){return(0,r.wg)(),(0,r.iD)("div",u,e)}var c=a(89);const o={},f=(0,c.Z)(o,[["render",s]]);var l=f}}]);
-//# sourceMappingURL=about.10bc8433.js.map
+//# sourceMappingURL=about.d6ec7ea1.js.map
